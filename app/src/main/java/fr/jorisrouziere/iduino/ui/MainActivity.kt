90a8638fa842aaa5b3e8d66c71d5affc933bfed2
Antoine <antoine.macheda@ynov.com>
2022-12-01 13:04:33 +0100

    NavBar

--- app/src/main/java/fr/jorisrouziere/iduino/ui/MainActivity.kt
+++ app/src/main/java/fr/jorisrouziere/iduino/ui/MainActivity.kt
@@ -2,11 +2,48 @@ package fr.jorisrouziere.iduino.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import fr.jorisrouziere.iduino.R
+import fr.jorisrouziere.iduino.databinding.ActivityMainBinding
+import fr.jorisrouziere.iduino.favorite
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(fr.jorisrouziere.iduino.Map())
+
+        binding.bottomNavigationView.setOnItemReselectedListener {
+
+            when(it.itemId){
+
+                R.id.list -> replaceFragment(fr.jorisrouziere.iduino.List())
+                R.id.map -> replaceFragment(fr.jorisrouziere.iduino.Map())
+                R.id.favoris -> replaceFragment(favorite())
+
+                else ->{
+
+
+                }
+
+            }
+            true
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
     }
 }
