bfb6a2e00c136078b66f433cc14b4b30532f48dd
f <f>
2022-12-01 11:21:09 +0100

    push simple map

--- app/src/main/java/fr/jorisrouziere/iduino/ui/MainActivity.kt
+++ app/src/main/java/fr/jorisrouziere/iduino/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package fr.jorisrouziere.iduino.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import fr.jorisrouziere.iduino.R
 
+
+// Implement OnMapReadyCallback.
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
