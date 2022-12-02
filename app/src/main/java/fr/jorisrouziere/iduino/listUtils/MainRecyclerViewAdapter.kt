ea2a5dd3ee314515a42ee7ad4e20e80adea56c73
joris <joris.rouziere@gmail.cm>
2022-12-02 13:04:00 +0100

    Liste des bars

---
+++ app/src/main/java/fr/jorisrouziere/iduino/listUtils/MainRecyclerViewAdapter.kt
@@ -0,0 +1,46 @@
+package fr.jorisrouziere.iduino.listUtils
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+
+import fr.jorisrouziere.iduino.databinding.ItemBarBinding
+import fr.jorisrouziere.iduino.model.Bar
+
+class MainRecyclerViewAdapter(
+    private val values: List<Bar>
+) : RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            ItemBarBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.nameView.text = item.name
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: ItemBarBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val nameView: TextView = binding.itemName
+        val carteButton: Button = binding.itemCarteButton
+        val favoriteButton: ImageButton = binding.itemFavoriteButton
+
+        override fun toString(): String {
+            return super.toString() + " '" + nameView.text + "'"
+        }
+    }
+
+}
--- app/src/main/java/fr/jorisrouziere/iduino/ui/MainFragment.kt
+++ app/src/main/java/fr/jorisrouziere/iduino/ui/MainFragment.kt
@@ -5,18 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fr.jorisrouziere.iduino.R
+import fr.jorisrouziere.iduino.listUtils.MainRecyclerViewAdapter
+import fr.jorisrouziere.iduino.model.Bar
 
 class MainFragment : Fragment() {
 
+    private var columnCount = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MainRecyclerViewAdapter(bars)
+            }
+        }
+        return view
+    }
+
+    companion object {
+        lateinit var bars: ArrayList<Bar>
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            FavorisFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
     }
 }
